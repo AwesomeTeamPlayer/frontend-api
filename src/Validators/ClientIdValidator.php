658af230d81af2e1950f522b26d10e10fdc19cff
SimonIsMe <szym1991@gmail.com>
2017-09-10 11:41:35 +0200

    Create ClientIdValidator

---
+++ src/Validators/ClientIdValidator.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Validators;
+
+use Validator\ValidatorInterface;
+
+class ClientIdValidator implements ValidatorInterface
+{
+	/**
+	 * @var int
+	 */
+	const CLIENT_ID_IS_VALID = 0;
+
+	/**
+	 * @var int
+	 */
+	const CLIENT_ID_IS_NOT_VALID = 1;
+
+	/**
+	 * @param mixed $value
+	 *
+	 * @return int -   0 if VALUE is correct
+	 *                 > 0 otherwise. Returned value specifies type of error.
+	 */
+	public function valid($value): int
+	{
+		if (preg_match('/^[a-z0-9]{32}\-([0-9\.]{7,15})\-[0-9]{1,}$/', $value, $out) !== 1) {
+			return self::CLIENT_ID_IS_NOT_VALID;
+		}
+
+		if ($this->validIpAddress($out[1]) === false) {
+			return self::CLIENT_ID_IS_NOT_VALID;
+		}
+
+		return self::CLIENT_ID_IS_VALID;
+	}
+
+	/**
+	 * @param string $ipAddress
+	 *
+	 * @return bool
+	 */
+	private function validIpAddress(string $ipAddress): bool
+	{
+		$parts = explode('.', $ipAddress);
+		if (count($parts) !== 4) {
+			return false;
+		}
+
+		foreach ($parts as $part) {
+			if ((int) $part > 256) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * @param int $validationResult
+	 *
+	 * @return string
+	 */
+	public function errorText(int $validationResult): string
+	{
+		switch ($validationResult) {
+			case self::CLIENT_ID_IS_VALID:
+				return 'Ok';
+			case self::CLIENT_ID_IS_NOT_VALID:
+				return 'Given value is not correct Client ID.';
+		}
+	}
+
+	/**
+	 * Returns unique name of the validator.
+	 *
+	 * @return string
+	 */
+	public function getName(): string
+	{
+		return 'ClientIdValidator';
+	}
+}
---
+++ tests/unit/Validators/ClientIdValidatorTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Validators;
+
+use PHPUnit\Framework\TestCase;
+
+class ClientIdValidatorTest extends TestCase
+{
+	public function test_getName()
+	{
+		$clientIdValidator = new ClientIdValidator();
+		$name = $clientIdValidator->getName();
+		$this->assertEquals('ClientIdValidator', $name);
+	}
+
+	/**
+	 * @dataProvider dataProvider_test_valid
+	 */
+	public function test_valid($clientId, $expectedResult, $expectedErrorText)
+	{
+		$clientIdValidator = new ClientIdValidator();
+		$result = $clientIdValidator->valid($clientId);
+
+		$this->assertEquals($expectedResult, $result);
+		$this->assertEquals($expectedErrorText, $clientIdValidator->errorText($result));
+	}
+
+	public function dataProvider_test_valid()
+	{
+		return [
+			[
+				'',
+				ClientIdValidator::CLIENT_ID_IS_NOT_VALID,
+				'Given value is not correct Client ID.',
+			],
+			[
+				$this->generateString(32),
+				ClientIdValidator::CLIENT_ID_IS_NOT_VALID,
+				'Given value is not correct Client ID.',
+			],
+			[
+				$this->generateString(32) . '-1.1.1.1',
+				ClientIdValidator::CLIENT_ID_IS_NOT_VALID,
+				'Given value is not correct Client ID.',
+			],
+			[
+				$this->generateString(32) . '-1.1.1.1-123',
+				ClientIdValidator::CLIENT_ID_IS_VALID,
+				'Ok',
+			],
+			[
+				$this->generateString(32) . '-abc-1',
+				ClientIdValidator::CLIENT_ID_IS_NOT_VALID,
+				'Given value is not correct Client ID.',
+			],
+			[
+				$this->generateString(32) . '-1.1.1.1-abc',
+				ClientIdValidator::CLIENT_ID_IS_NOT_VALID,
+				'Given value is not correct Client ID.',
+			],
+			[
+				$this->generateString(32) . '-1.1.1.1-' . $this->generateString(100, '1') ,
+				ClientIdValidator::CLIENT_ID_IS_VALID,
+				'Ok',
+			],
+			[
+				$this->generateString(32) . '-192.168.1.1-123',
+				ClientIdValidator::CLIENT_ID_IS_VALID,
+				'Ok',
+			],
+			[
+				$this->generateString(32) . '-1.1.1.1.1-1',
+				ClientIdValidator::CLIENT_ID_IS_NOT_VALID,
+				'Given value is not correct Client ID.',
+			],
+			[
+				$this->generateString(32) . '-192.168.1.257-123',
+				ClientIdValidator::CLIENT_ID_IS_NOT_VALID,
+				'Given value is not correct Client ID.',
+			],
+		];
+	}
+
+	private function generateString(int $stringLength, string $char = 'a'): string
+	{
+		$string = '';
+		for ($i = 0; $i < $stringLength; $i++) {
+			$string .= $char;
+		}
+
+		return $string;
+	}
+}
