2c59ee5eac55e214c0d7b861da94ee8e253b87e3
SimonIsMe <szym1991@gmail.com>
2017-09-10 10:39:06 +0200

    Create empty ProjectGetUserForProjectEndpoint

---
+++ src/Endpoints/ProjectGetUserForProjectEndpoint.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Endpoints;
+
+use Validator\ArrayValidator;
+use Validator\IsIntegerValidator;
+use Validator\IsNotNullValidator;
+use Validator\IsNumberGreaterThanValidator;
+use Validator\IsStringValidator;
+use Validator\StringLengthValidator;
+use Validator\ValidationResult;
+use Validator\ValidatorsCollection;
+
+class ProjectGetUserForProjectEndpoint extends AbstractEndpoint
+{
+	protected function validate(array $data): ValidationResult
+	{
+		$validators = [
+			'projectId' => new ValidatorsCollection([
+				new IsNotNullValidator(),
+				new IsStringValidator(),
+				new StringLengthValidator(5),
+			]),
+			'limit' => new ValidatorsCollection([
+				new IsNotNullValidator(),
+				new IsIntegerValidator(),
+				new IsNumberGreaterThanValidator(0)
+			]),
+			'page' => new ValidatorsCollection([
+				new IsNotNullValidator(),
+				new IsIntegerValidator(),
+				new IsNumberGreaterThanValidator(0)
+			]),
+		];
+
+		$arrayValidator = new ArrayValidator();
+		return $arrayValidator->validateArray($validators, $data);
+	}
+
+	protected function run(array $data)
+	{
+
+	}
+}
