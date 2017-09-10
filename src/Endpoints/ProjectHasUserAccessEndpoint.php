257a782cbbc98f83ae52b07275d76ce17ef80039
SimonIsMe <szym1991@gmail.com>
2017-09-10 10:39:16 +0200

    Create empty ProjectHasUserAccessEndpoint

---
+++ src/Endpoints/ProjectHasUserAccessEndpoint.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Endpoints;
+
+use Validator\ArrayValidator;
+use Validator\IsIntegerValidator;
+use Validator\IsNotNullValidator;
+use Validator\IsNumberGreaterThanValidator;
+use Validator\IsStringValidator;
+use Validator\StringLengthValidator;
+use Validator\ValidationResult;
+use Validator\ValidatorsCollection;
+
+class ProjectHasUserAccessEndpoint extends AbstractEndpoint
+{
+	protected function validate(array $data): ValidationResult
+	{
+		$validators = [
+			'userId' => new ValidatorsCollection([
+				new IsNotNullValidator(),
+				new IsIntegerValidator(),
+				new IsNumberGreaterThanValidator(0)
+			]),
+			'projectId' => new ValidatorsCollection([
+				new IsNotNullValidator(),
+				new IsStringValidator(),
+				new StringLengthValidator(5),
+			]),
+		];
+
+		$arrayValidator = new ArrayValidator();
+		return $arrayValidator->validateArray($validators, $data);
+	}
+
+	protected function run(array $data)
+	{
+
+	}
+}
